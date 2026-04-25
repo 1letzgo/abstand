10ba1464b904b3e2489e0484341e8a94b8715321
letzgo <daniel@letzgo.me>
2026-04-25 19:35:56 +0200

    Initial Commit

---
+++ abstand/abstandApp.swift
@@ -0,0 +1,17 @@
+//
+//  abstandApp.swift
+//  abstand
+//
+//  Created by Daniel Goletz on 25.04.26.
+//
+
+import SwiftUI
+
+@main
+struct abstandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
